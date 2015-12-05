831f2fd6e1fe01140dfd2ce6de301e6798f90ec4
peterlowe42 <peter.lowe42@gmail.com>
2015-12-05 11:34:34 -0700

    Adds files for pairing session 4.7

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,25 @@
+# Factorial
+
+# I worked on this challenge [by myself, with: ].
+=begin
+create new variable num equal to input-1
+multiply input by num
+subract 1 from num
+repeat until num = 0
+=end
+
+def factorial(input)
+	if input == 0 
+		return 1
+	end
+  num = input - 1 
+  result = input 
+   
+  while num >= 1
+    result = result * num
+    num -= 1
+  end
+  return result
+end
+
+
