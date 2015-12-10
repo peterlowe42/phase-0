8b653ff49c4f7410ca87966571ce0a2eb470a7e3
peterlowe42 <peter.lowe42@gmail.com>
2015-12-10 13:21:16 -0700

    Adds solution to die-class

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -19,8 +19,10 @@
 
 class Die
   def initialize(sides)
-  	
-    @sides = sides
+    if sides < 1
+      raise ArgumentError 
+    end
+  	    @sides = sides 
   end
 
   def sides
@@ -28,7 +30,7 @@ class Die
   end
 
   def roll
-    r = rand(@sides)
+    r = rand(1...(@sides + 1))
     return r
   end
 
@@ -36,9 +38,51 @@ end
 
 
 
-
 # 3. Refactored Solution
 
+class Die
+  def initialize(sides)
+    raise ArgumentError if sides < 1
+      @sides = sides 
+  end
+
+  def sides
+    return @sides
+  end
+
+  def roll
+    return rand(1...(@sides + 1))
+  end
+
+end
+
+=begin
+What is an ArgumentError and why would you use one?
+
+  An ArgumentError is an error that is raised when the input argument(s) for a method do not meet the expected criteria.  In this case the error is raised if 
+  sides is < 1.  You use ArgumentErrors to ensure that the wrong type of input cannot be fed into your method by a user. 
+
+What new Ruby methods did you implement? What challenges and successes did you have in implementing them?
+
+  I used rand for the first time. Initally I had some trouble understanding what input I needed for it. I tried just rand(@sides) first but this returned 
+  0 as a possible value and only went up to @sides-1.  After a little research I found how to set the possible range of outputs to what i wanted. 
+
+What is a Ruby class?
+
+  A rubt class is a 'type' of ruby object. For example String, Array and Hash are all ruby classes. 
+
+Why would you use a Ruby class?
+
+  If you want to create a new type of object that has specific characteristics and methods. 
+
+What is the difference between a local variable and an instance variable?
+
+  A local variable only exists within the method. An instance variable exixts globally
+
+Where can an instance variable be used?
+  
+  Within 'initialize'
+=end
 
 
 
